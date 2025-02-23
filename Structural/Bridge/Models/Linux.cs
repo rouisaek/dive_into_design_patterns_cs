8e4fae472539d8b01c4612c7d70b22ea2bad3865
rouisaek <rouisaek@outlook.com>
2025-02-23 18:15:39 +0100

    Show System Information

--- Structural/Bridge/Models/Linux.cs
+++ Structural/Bridge/Models/Linux.cs
@@ -6,20 +6,29 @@ namespace Bridge.Models
     {
         public void Init()
         {
+            Console.Clear();
             int loop = 0;
-
             Console.CursorVisible = false;
             while (loop < 5)
             {
                 for (int i = 0; i < 5; i++)
                 {
                     Console.Write(".");
-                    Thread.Sleep(500);
+                    Thread.Sleep(200);
                 }
                 Console.Clear();
                 loop++;
             }
+            ShowSystemInformation();
             Console.CursorVisible = true;
+        }
+
+        private void ShowSystemInformation()
+        {
+            Console.WriteLine("Username: " + Environment.UserName);
+            Console.WriteLine("Processor Count: " + Environment.ProcessorCount);
+            Console.WriteLine("OS Version: " + Environment.OSVersion);
+            Console.WriteLine("Machine Name: " + Environment.MachineName);
         }
 
     }
